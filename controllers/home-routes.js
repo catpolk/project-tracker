461e053bb8cc9b06b288af2e0f83cbec13c3d646
JustinSnyder611 <JustinSnyder611@gmail.com>
2022-11-07 19:25:39 -0600

    Added routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -11,4 +11,44 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.get('/progress', async (req, res) => {
+  try {
+    res.render('progress', {});
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/roadmap', async (req, res) => {
+  try {
+    res.render('roadmap', {});
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/planning', async (req, res) => {
+  try {
+    res.render('planning', {});
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/users', async (req, res) => {
+  try {
+    res.render('users', {});
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/login', async (req, res) => {
+  try {
+    res.render('login', {});
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
