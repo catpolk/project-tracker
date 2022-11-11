8ef676585389898023bdea907ab1771f1b98ed9d
Catherine <cpolknytx@gmail.com>
2022-11-10 22:47:25 -0600

    added progress pie chart, handlebars routes

--- models/index.js
+++ models/index.js
@@ -1,10 +1,13 @@
 const Time = require('./Time');
 const Tasks = require('./Tasks');
 const Users = require('./Users');
+const User = require('./Users');
 
 Users.hasMany(Tasks, {
     foreignKey: 'user_id',
 });
+
+Tasks.belongsTo(Users);
 
 
 
