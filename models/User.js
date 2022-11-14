2179aa272a2fc2ba5833e7f1619edce3c032a0c8
Catherine <cpolknytx@gmail.com>
2022-11-14 12:51:49 -0600

    added comments

--- models/User.js
+++ models/User.js
@@ -2,6 +2,7 @@ const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
+// Create a new Sequelize model for user
 class User extends Model {
   checkPassword(userPassword) {
     return bcrypt.compareSync(userPassword, this.password);
